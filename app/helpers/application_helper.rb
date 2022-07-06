98be31cfc41e4173cb86242424c9b4d7103b0f8b
Igor Asamadzinau <igor.asamadzinau@checkr.com>
2022-07-05 21:51:32 -0400

    Index page for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-  def gravatar_for(user, options = { size: 200, style: 'robohash' })
+  def gravatar_for(user, options = { size: 200 })
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
     size = options[:size]
     style = options[:style]
-    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=#{style}&r=x"
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=robohash&r=x"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
 end
