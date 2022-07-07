bd194355c4c818c76de5a176ca52b05f1875082a
Igor Asamadzinau <igor.asamadzinau@checkr.com>
2022-07-07 16:29:11 -0400

    flash notice. Style helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :flash_style
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,6 +14,15 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       flash[:alert] = "You must be logged in to perform that action"
       redirect_to root_path
+    end
+  end
+
+  def flash_style(level)
+    case level
+    when "notice" then "info"
+    when "success" then "success"
+    when "error" then "danger"
+    when "alert" then "warning"
     end
   end
 end
