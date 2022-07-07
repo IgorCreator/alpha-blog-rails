e5e571a9fb5145753cabe0320542658194c011af
Igor Asamadzinau <igor.asamadzinau@checkr.com>
2022-07-07 15:08:30 -0400

    Validations and restrictions for users and articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
 
-  helper_method :current_user
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :logged_in?
   def logged_in?
     # transformed User obj in bool with "!!"
     !!current_user
+  end
+
+  def required_user
+    unless logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
   end
 end
