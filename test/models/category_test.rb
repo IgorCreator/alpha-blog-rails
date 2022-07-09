f8a5fb8783915b02d4d29e3f892e345d7741fad3
Igor Asamadzinau <igor.asamadzinau@checkr.com>
2022-07-09 13:19:52 -0400

    Add category model validations using unit tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,33 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-  test "category should be valid" do
+  def setup
     @category = Category.new(name: "sports")
+  end
+
+  test "category should be valid" do
     assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "sports")
+    assert_not @category2.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "or"
+    assert_not @category.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "very long name for testing. unreal long name"
+    assert_not @category.valid?
   end
 
 end
