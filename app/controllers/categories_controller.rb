610363adff8bdc85543a90de43e53c16e62e1dd1
Igor Asamadzinau <igor.asamadzinau@checkr.com>
2022-07-09 23:30:34 -0400

    Complete categories create action and functional test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,34 @@
 class CategoriesController < ApplicationController
 
-  def new
-
+  def show
   end
 
   def index
-
   end
 
-  def show
+  def new
+    @category = Category.new
+  end
 
+  def edit
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "New category [##{@category.name}] was created successfully."
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
